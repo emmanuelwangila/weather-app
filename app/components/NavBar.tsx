607089a526f7ffada6c0e0606b9415121a4cac88
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-02-09 15:01:51 +0300

    feat fix styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
+import { IoSunny } from "react-icons/io5";
+import { MdLocationSearching } from "react-icons/md";
+
 type Props = {};
 
 export default function NavBar({}: Props) {
   return (
-    <nav className="bg-white rounded-md sticky top-0  m-2 p-3  lg:w-[95%] ">
-      <div>
+    <nav className="bg-white flex flex-row  justify-between rounded-md sticky top-0  m-2 p-3  lg:w-[95%] ">
+      <div className="flex flex-row  gap-4 ">
         <h1 className="text-blue-500 font-bold">Weather-app</h1>
+        <IoSunny className="text-green-500 text-4xl    " />
+      </div>
+
+      <div className="flex justify-end  ">
+        <MdLocationSearching className="flex text-3xl cursor-pointer hover:bg-green-400  text-green-500" />
       </div>
     </nav>
   );
